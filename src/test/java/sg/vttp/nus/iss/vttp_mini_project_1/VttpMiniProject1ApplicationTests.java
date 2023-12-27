d70d5d312c48998cfa1907be107c341abbd50d34
JD-KE <60773497+JD-KE@users.noreply.github.com>
2023-12-27 19:54:24 +0800

    first commit

---
+++ src/test/java/sg/vttp/nus/iss/vttp_mini_project_1/VttpMiniProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.vttp.nus.iss.vttp_mini_project_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VttpMiniProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
